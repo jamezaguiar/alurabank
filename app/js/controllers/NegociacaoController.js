c7026cff1b6953fcb6224b7980a870135657d0e2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-02-26 15:31:26 -0300

    Template dinâmico

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,16 +1,16 @@
 class NegociacaoController {
     constructor() {
         this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoesView');
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = (document.querySelector('#quantidade'));
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
         const negociacao = new Negociacao(new Date(this._inputData.value), Number(this._inputQuantidade.value), Number(this._inputValor.value));
         this._negociacoes.adiciona(negociacao);
-        this._negociacoes.paraArray().forEach((negociacao) => {
-            console.log(negociacao);
-        });
+        this._negociacoesView.update(this._negociacoes);
     }
 }
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -5,12 +5,16 @@ class NegociacaoController {
 
   private _negociacoes = new Negociacoes();
 
+  private _negociacoesView = new NegociacoesView('#negociacoesView');
+
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector('#data');
     this._inputQuantidade = <HTMLInputElement>(
       document.querySelector('#quantidade')
     );
     this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+
+    this._negociacoesView.update(this._negociacoes);
   }
 
   adiciona(event: Event) {
@@ -24,8 +28,6 @@ class NegociacaoController {
 
     this._negociacoes.adiciona(negociacao);
 
-    this._negociacoes.paraArray().forEach((negociacao) => {
-      console.log(negociacao);
-    });
+    this._negociacoesView.update(this._negociacoes);
   }
 }
