34434eff1711207030371dc3624bd2a72e6bda64
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-01 09:12:34 -0300

    Carregamento de módulos

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,7 +1,17 @@
-import { View } from './View';
-export class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
       <table class="table table-hover table-bordered">
         <thead>
           <tr>
@@ -14,8 +24,8 @@ export class NegociacoesView extends View {
   
         <tbody>
           ${model
-            .paraArray()
-            .map((negociacao) => `
+                        .paraArray()
+                        .map((negociacao) => `
               <tr>
                 <td>
                   ${negociacao.data.getDate() + 1}
@@ -29,12 +39,16 @@ export class NegociacoesView extends View {
                 <td>${negociacao.volume}</td>
               </tr>
             `)
-            .join('')}
+                        .join('')}
         </tbody>
   
         <tfoot>
         </tfoot>
       </table>
       `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
