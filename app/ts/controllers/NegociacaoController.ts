257aed4b03a6b2ba62336c529b55ea66f5a18a90
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-01 08:55:29 -0300

    Namespaces

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -5,8 +5,8 @@ class NegociacaoController {
 
   private _negociacoes = new Negociacoes();
 
-  private _negociacoesView = new NegociacoesView('#negociacoesView');
-  private _mensagemView = new MensagemView('#mensagemView');
+  private _negociacoesView = new Views.NegociacoesView('#negociacoesView');
+  private _mensagemView = new Views.MensagemView('#mensagemView');
 
   constructor() {
     this._inputData = $('#data');
